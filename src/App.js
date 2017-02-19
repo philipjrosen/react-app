aaa740ecfa7f9feda9ff7f2159137899308fc4c0
Philip Rosen <philipjrosen@gmail.com>
2017-02-18 16:17:30 -0800

    Display Output in React with a Component's render Method

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 
 class App extends React.Component {
   render() {
-    return <h1>Hello World</h1>
+    return (
+    <h1>Hello World</h1>
+    );
   }
 }
 
